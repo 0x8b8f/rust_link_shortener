9e1443379d8da7aa11548ee5edd9b69fb3e7347c
0x8b8f <148609288+0x8b8f@users.noreply.github.com>
2023-10-21 08:11:45 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-  println!("Hello link shortener!");
+use axum::{response::Html, routing::get, Router, Server};
+
+#[tokio::main]
+pub async fn main() {
+    let router: Router = Router::new().route("/", get(|| async { Html("<h1>Url Shortener</h1>") }));
+
+    Server::bind(&"127.0.0.1:8080".parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
