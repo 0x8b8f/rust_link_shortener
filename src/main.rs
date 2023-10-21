588246452ee90ba943077b408e9a6acd95694198
rattevanessa <148609485+rattevanessa@users.noreply.github.com>
2023-10-21 09:26:06 +0200

    Update main.rs
    Otherwise server wont be reachable through docker

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use axum::{response::Html, routing::get, Router, Server};
 pub async fn main() {
     let router: Router = Router::new().route("/", get(|| async { Html("<h1>Url Shortener</h1>") }));
 
-    Server::bind(&"127.0.0.1:8080".parse().unwrap())
+    Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(router.into_make_service())
         .await
         .unwrap();
