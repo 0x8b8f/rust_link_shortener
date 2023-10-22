96c4580df8eeb68d13a015d65171f9d1642f1575
0x8b8f <148609288+0x8b8f@users.noreply.github.com>
2023-10-22 14:37:26 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use axum::{response::Html, routing::get, Router, Server};
+use axum::{response::Html, response::IntoResponse, routing::get, routing::post, routing::delete, Router, Server};
+
+pub async fn create() -> impl IntoResponse {
+    "create"
+}
+
+pub async fn go() -> impl IntoResponse {
+    "go"
+}
+
+pub async fn remove() -> impl IntoResponse {
+    "remove"
+}
 
 #[tokio::main]
 pub async fn main() {
-    let router: Router = Router::new().route("/", get(|| async { Html("<h1>Url Shortener</h1>") }));
+    let router: Router = Router::new().route("/", get(|| async { Html("<h1>Url Shortener</h1>") }))
+        .route("/create", post(create))
+        .route("/go", get(go))
+        .route("/remove", delete(remove));
 
     Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(router.into_make_service())
